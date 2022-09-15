dc89c21493aab135eb25ad759709300c7a9ba350
kimnakyung <35446851+dd6679@users.noreply.github.com>
2022-09-15 10:43:46 +0900

    .

--- Assets/NK/Scripts/NK_Move.cs
+++ Assets/NK/Scripts/NK_Move.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,16 @@ public class NK_Move : MonoBehaviour
     public Transform camPos;
     GameObject objectHitPosition;
     RaycastHit hitRay, hitLayerMask;
+
+    private void Update()
+    {
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        
+    }
 
     private void OnMouseUp()
     {
