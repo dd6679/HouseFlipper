432f151228c080d450baa5f97170b580387cd0d8
kimnakyung <35446851+dd6679@users.noreply.github.com>
2022-09-19 06:26:42 +0900

    .

--- Assets/NK/Scripts/NK_QuestUI.cs
+++ Assets/NK/Scripts/NK_QuestUI.cs
@@ -10,14 +10,22 @@ public class NK_QuestUI : MonoBehaviour
     public Text questFactory;
     public Image questParent;
     public static Dictionary<string, List<string>> quests = new Dictionary<string, List<string>> {
-        {"현관", new List<string>{"먼지 닦기", "페인트 칠하기"} },
-        {"방", new List<string>{"쓰레기 버리기", "먼지 닦기", "페인트 칠하기"} },
-        { "거실", new List<string> { "쓰레기 버리기", "먼지 닦기", "벽 철거하기", "페인트 칠하기" } },
-        {"주방", new List<string>{"쓰레기 버리기", "먼지 닦기" } },
-        {"욕실", new List<string>{"먼지 닦기", "벽 철거하기"} },
-        {"다용도실", new List<string>{"쓰레기 버리기", "벽 철거하기"} },
+        {"현관 14㎡", new List<string>{"먼지 닦기", "페인트 칠하기"} },
+        {"방 25㎡", new List<string>{"쓰레기 버리기", "먼지 닦기", "페인트 칠하기"} },
+        {"방 18㎡", new List<string>{"쓰레기 버리기", "먼지 닦기", "페인트 칠하기"} },
+        {"방 15㎡", new List<string>{"쓰레기 버리기", "먼지 닦기", "페인트 칠하기"} },
+        {"방 45㎡", new List<string>{"쓰레기 버리기", "먼지 닦기", "페인트 칠하기"} },
+        { "거실 45㎡", new List<string> { "쓰레기 버리기", "먼지 닦기", "벽 철거하기", "페인트 칠하기" } },
+        { "거실 25㎡", new List<string> { "쓰레기 버리기", "먼지 닦기", "벽 철거하기", "페인트 칠하기" } },
+        {"주방 22㎡", new List<string>{"쓰레기 버리기", "먼지 닦기" } },
+        {"욕실 5㎡", new List<string>{"먼지 닦기", "벽 철거하기"} },
+        {"욕실 10㎡", new List<string>{"먼지 닦기", "벽 철거하기"} },
+        {"욕실 13㎡", new List<string>{"먼지 닦기", "벽 철거하기"} },
+        {"욕실 8㎡", new List<string>{"먼지 닦기", "벽 철거하기"} },
+        {"다용도실 8㎡", new List<string>{"쓰레기 버리기", "벽 철거하기"} },
         { "밖", new List<string> { "쓰레기 버리기" } },
-        {"차고", new List<string>{"쓰레기 버리기", "페인트 칠하기" } } };
+        { "복도 12㎡", new List<string> { "벽 철거하기" } },
+        {"차고 42㎡", new List<string>{"쓰레기 버리기", "페인트 칠하기" } } };
 
     private void OnTriggerEnter(Collider other)
     {
@@ -34,6 +42,8 @@ public class NK_QuestUI : MonoBehaviour
             }
 
             location.text = locationName;
+
+
 
             for (int i = 0; i < quests[location.text].Count; i++)
             {
