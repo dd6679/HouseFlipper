0c35194d157835654f969da8a76cc6eb1fb93af6
Ndongkyun <108320572+Ndongkyun@users.noreply.github.com>
2022-09-19 00:29:26 +0900

    09.19 DK
    .

--- Assets/Scripts/TrashDelete.cs
+++ Assets/Scripts/TrashDelete.cs
@@ -11,33 +11,27 @@ public class TrashDelete : MonoBehaviour
 
 
     RaycastHit hit;
-    float timer;
-    float timer2 = 1f;
     Transform tr;
+
     void Update()
     {
-        if (tr != null)
-        {
-            tr.localScale -= new Vector3(0.1f, 0.1f, 0.1f);
-            if (tr.localScale.x <= 0.1f)
-            {
-                Destroy(tr.gameObject);
-            }
-        }
         if (Input.GetMouseButton(0))
         {
 
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
             {
-                //tr = hit.collider.gameObject.GetComponent<Transform>();
                 if (hit.collider.gameObject.tag.Contains("Trash"))
                 {
-
                     tr = hit.collider.gameObject.transform;
-                //hit.collider.gameObject.transform.localScale -= new Vector3(0.01f, 0.01f, 0.01f);
-
-            
                 }
+            }
+        }
+        if (tr != null)
+        {
+            tr.localScale -= new Vector3(0.05f, 0.05f, 0.05f);
+            if (tr.localScale.x <= 0.1f)
+            {
+                Destroy(tr.gameObject);
             }
         }
     }
