2f4a0aae9520929cb00a51aed062d19df7289120
Ndongkyun <108320572+Ndongkyun@users.noreply.github.com>
2022-09-25 17:45:40 +0900

    .
    .

--- Assets/DK/StoreUI.cs
+++ Assets/DK/StoreUI.cs
@@ -8,21 +8,40 @@ public class StoreUI : MonoBehaviour
 {
     public GameObject[] Bt;
     public GameObject[] Fur;
-    public int index = -1;
+    int index = -1;
 
     GameObject UI;
     RaycastHit hit;
 
+
+    GameObject Furni;
+    GameObject Bath;
+    GameObject Paint;
+    Button instBt;
+
+
+    private void Awake()
+    {
+        UI = GameObject.Find("StoreUI");
+        Furni = GameObject.Find("Fur");
+        Bath = GameObject.Find("Bath");
+        Paint = GameObject.Find("Paint");
+        instBt = GameObject.Find("instBt").GetComponent<Button>();
+    }
+
     void Start()
     {
-       UI = GameObject.Find("StoreUI");
 
         //UI = gameObject.GetComponentInParent<Image>();
+        Paint.SetActive(false);
+        Bath.SetActive(false);
+
+
     }
 
     void Update()
     {
-        print(index);
+        //print(index);
     }
 
     public void Onclick()
@@ -32,33 +51,47 @@ public class StoreUI : MonoBehaviour
         {
             if(Bt[i] == clickObject)
             {
+                print(index);
                 index = i;
             }
         }
     }
 
-
-    public void Go0()
-    {
-        print("11");
-
-        index = 0;
-
-
-        //Cursor.visible = false;
-        //Cursor.lockState = CursorLockMode.Locked;
-
-
-        //UI.gameObject.SetActive(false);
-
-        GameObject gogo = Instantiate(Fur[index]);
-        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
-        if (Physics.Raycast(ray, out hit))
-        {
-                gogo.transform.position = hit.point;
-        }
-    }
     
+
+    public void FurBt()
+    {
+        Furni.SetActive(true);
+
+        Bath.SetActive(false);
+        Paint.SetActive(false);
+        instBt.onClick.SetPersistentListenerState(0, UnityEngine.Events.UnityEventCallState.RuntimeOnly);
+        instBt.onClick.SetPersistentListenerState(1, UnityEngine.Events.UnityEventCallState.Off);
+        instBt.onClick.SetPersistentListenerState(2, UnityEngine.Events.UnityEventCallState.Off);
+    }
+
+    public void BathBt()
+    {
+        Bath.SetActive(true);
+        
+        Furni.SetActive(false);
+        Paint.SetActive(false);
+        instBt.onClick.SetPersistentListenerState(0, UnityEngine.Events.UnityEventCallState.Off);
+        instBt.onClick.SetPersistentListenerState(1, UnityEngine.Events.UnityEventCallState.RuntimeOnly);
+        instBt.onClick.SetPersistentListenerState(2, UnityEngine.Events.UnityEventCallState.Off);
+    }
+
+    public void PaintBt()
+    {
+        Paint.SetActive(true);
+
+        Bath.SetActive(false);
+        Furni.SetActive(false);
+        instBt.onClick.SetPersistentListenerState(0, UnityEngine.Events.UnityEventCallState.Off);
+        instBt.onClick.SetPersistentListenerState(1, UnityEngine.Events.UnityEventCallState.Off);
+        instBt.onClick.SetPersistentListenerState(2, UnityEngine.Events.UnityEventCallState.RuntimeOnly);
+    }
+
 
     public void Instantiate(int i)
     {
