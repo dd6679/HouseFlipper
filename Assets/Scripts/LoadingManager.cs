e0eccaa44275d77c7734562e3389e5d612779e3a
Ndongkyun <108320572+Ndongkyun@users.noreply.github.com>
2022-10-03 00:22:51 +0900

    .
    .

--- Assets/Scripts/LoadingManager.cs
+++ Assets/Scripts/LoadingManager.cs
@@ -2,18 +2,42 @@ using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoadingManager : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
+    public Slider loadSlider;
+
+    public GameObject instructor;
+    public GameObject mento;
+
+    public GameObject imbage1;
+    public GameObject imbage2;
+    
+
+
+
     void Start()
     {
-        
+        loadSlider.minValue = 0;
+        loadSlider.maxValue = 10;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        loadSlider.value += Time.deltaTime;
+
+        if(loadSlider.value > 5)
+        {
+            instructor.SetActive(false);
+            imbage1.SetActive(false);
+            mento.SetActive(true);
+            imbage2.SetActive(true);
+        }
+        if(loadSlider.value > 8)
+        {
+            PhotonNetwork.LoadLevel(3);
+        }
 
     }
 
