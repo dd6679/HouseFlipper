cd529da5f92877b9bf038c1f663bf61ecf4502ac
Ndongkyun <108320572+Ndongkyun@users.noreply.github.com>
2022-10-02 14:34:16 +0900

    .
    .

--- Assets/NK/Scripts/NK_TaskUI.cs
+++ Assets/NK/Scripts/NK_TaskUI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
-using static UnityEditor.FilePathAttribute;
+//using static UnityEditor.FilePathAttribute;
 
 public class NK_TaskUI : MonoBehaviour
 {
